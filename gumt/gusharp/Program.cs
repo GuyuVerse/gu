01a0c7a1b30a7757c60feebc63058989636abdbb
剧毒的KCN <103011451+JDDKCN@users.noreply.github.com>
2023-06-23 15:26:47 +0800

    Upload Gusharp

---
+++ gumt/gusharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GuPackage.GuPackage;
+
+namespace Gusharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GuString s1 = new GuString(new char[] { 'H', 'e', 'l', 'l', 'o' });
+            GuString s2 = new GuString(new char[] { 'W', 'o', 'r', 'l', 'd' });
+            GuString s3 = s1.GuConcat(s2);
+
+            GuPrint(s3.ToGuString());
+        }
+
+        
+    }
+}
